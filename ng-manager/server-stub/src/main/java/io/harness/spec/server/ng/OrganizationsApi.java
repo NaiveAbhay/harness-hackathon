ec7015313295a26b563265d46a47c852789526d4
Ashish Sanodia <ashish.sanodia@harness.io>
2022-05-30 14:58:08 +0530

    [PL-25514]: Adds generated server stub for ng-manager

---
+++ ng-manager/server-stub/src/main/java/io/harness/spec/server/ng/OrganizationsApi.java
@@ -0,0 +1,103 @@
+package io.harness.spec.server.ng;
+
+import io.harness.spec.server.ng.model.CreateOrganizationRequest;
+import io.harness.spec.server.ng.model.OrganizationResponse;
+import io.harness.spec.server.ng.model.UpdateOrganizationRequest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+import java.util.Map;
+import java.util.List;
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+@Path("/v1/organizations")
+
+public interface OrganizationsApi {
+
+    @POST
+    @Consumes({ "application/json" })
+    @Produces({ "application/json" })
+    @Operation(summary = "Create Organization", description = "Create a new Organization.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Organization" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Organization response", content = @Content(mediaType = "application/json", schema = @Schema(implementation = OrganizationResponse.class))) })
+    OrganizationResponse createOrganization(@Valid CreateOrganizationRequest body,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+);
+    @DELETE
+    @Path("/{id}")
+    @Produces({ "application/json" })
+    @Operation(summary = "Delete Organization", description = "Delete the information of the organization with the matching organization identifier.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Organization" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Organization response", content = @Content(mediaType = "application/json", schema = @Schema(implementation = OrganizationResponse.class))) })
+    OrganizationResponse deleteOrganization( @PathParam("id")
+
+ @Parameter(description = "Organization identifier") String id
+,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+);
+    @GET
+    @Path("/{id}")
+    @Produces({ "application/json" })
+    @Operation(summary = "Get Organization", description = "Retrieve the information of the organization with the matching organization identifier.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Organization" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Organization response", content = @Content(mediaType = "application/json", schema = @Schema(implementation = OrganizationResponse.class))) })
+    OrganizationResponse getOrganization( @PathParam("id")
+
+ @Parameter(description = "Organization identifier") String id
+,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+);
+    @GET
+    @Produces({ "application/json" })
+    @Operation(summary = "Get Organizations", description = "Retrieve the information of the Organizations.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Organization" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Organization lsit response", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = OrganizationResponse.class)))) })
+    List<OrganizationResponse> getOrganizations(  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+,  @QueryParam("org") 
+
+ @Parameter(description = "Comma separated list of Organizations")  String org
+,  @QueryParam("search_term") 
+
+ @Parameter(description = "This would be used to filter resources having attributes matching with search term.")  String searchTerm
+,  @QueryParam("page") @DefaultValue("1") 
+
+ @Parameter(description = "Pagination page number strategy: Specify the page number within the paginated collection related to the number of items in each page ")  Integer page
+,  @QueryParam("limit") @DefaultValue("30") 
+
+ @Parameter(description = "Pagination: Number of items to return")  Integer limit
+);
+    @PUT
+    @Path("/{id}")
+    @Consumes({ "application/json" })
+    @Produces({ "application/json" })
+    @Operation(summary = "Update Organization", description = "Update the information of the organization with the matching organization identifier.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Organization" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Organization response", content = @Content(mediaType = "application/json", schema = @Schema(implementation = OrganizationResponse.class))) })
+    OrganizationResponse updateOrganization( @PathParam("id")
+
+ @Parameter(description = "Organization identifier") String id
+,@Valid UpdateOrganizationRequest body,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+);}
---
+++ ng-manager/server-stub/src/main/java/io/harness/spec/server/ng/ProjectsApi.java
@@ -0,0 +1,125 @@
+package io.harness.spec.server.ng;
+
+import io.harness.spec.server.ng.model.CreateProjectRequest;
+import io.harness.spec.server.ng.model.ProjectResponse;
+import io.harness.spec.server.ng.model.UpdateProjectRequest;
+
+import javax.ws.rs.*;
+import javax.ws.rs.core.Response;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+import java.util.Map;
+import java.util.List;
+import javax.validation.constraints.*;
+import javax.validation.Valid;
+
+@Path("/v1/projects")
+
+public interface ProjectsApi {
+
+    @POST
+    @Path("/org/{org}")
+    @Consumes({ "application/json", "application/yaml" })
+    @Produces({ "application/json", "application/yaml" })
+    @Operation(summary = "Create Project", description = "Creates a new Project", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Project" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Project response", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProjectResponse.class))) })
+    ProjectResponse createProject( @PathParam("org")
+
+ @Parameter(description = "Slug field of the organization the resource is scoped to") String org
+,@Valid CreateProjectRequest body,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+);
+    @DELETE
+    @Path("/org/{org}/project/{id}")
+    @Produces({ "application/json", "application/yaml" })
+    @Operation(summary = "Delete Project", description = "Deletes the information of the Project with the matching Project identifier.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Project" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Project response", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProjectResponse.class))) })
+    ProjectResponse deleteProject( @PathParam("id")
+
+ @Parameter(description = "Project identifier") String id
+, @PathParam("org")
+
+ @Parameter(description = "Slug field of the organization the resource is scoped to") String org
+,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+);
+    @GET
+    @Path("/org/{org}/project/{id}")
+    @Produces({ "application/json", "application/yaml" })
+    @Operation(summary = "Get Project", description = "Retrieve the information of the Project with the matching Project identifier.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Project" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Project response", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProjectResponse.class))) })
+    ProjectResponse getProject( @PathParam("id")
+
+ @Parameter(description = "Project identifier") String id
+, @PathParam("org")
+
+ @Parameter(description = "Slug field of the organization the resource is scoped to") String org
+,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+);
+    @GET
+    @Produces({ "application/json" })
+    @Operation(summary = "Get Projects", description = "Retrieves the information of the Projects.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Project" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Project list response", content = @Content(mediaType = "application/json", array = @ArraySchema(schema = @Schema(implementation = ProjectResponse.class)))) })
+    List<ProjectResponse> getProjects(  @QueryParam("project") 
+
+ @Parameter(description = "Comma separated list of Projects")  String project
+,  @QueryParam("org") 
+
+ @Parameter(description = "Comma separated list of Organizations")  String org
+,  @QueryParam("has_module") @DefaultValue("true") 
+
+ @Parameter(description = "This boolean specifies whether to Filter Projects which has the Module of type passed in the moduleType parameter or not")  Boolean hasModule
+,  @QueryParam("module_type") 
+
+ @Parameter(description = "Project&#x27;s module type")  Object moduleType
+,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+,  @QueryParam("search_term") 
+
+ @Parameter(description = "This would be used to filter resources having attributes matching with search term.")  String searchTerm
+,  @QueryParam("page") @DefaultValue("1") 
+
+ @Parameter(description = "Pagination page number strategy: Specify the page number within the paginated collection related to the number of items in each page ")  Integer page
+,  @QueryParam("limit") @DefaultValue("30") 
+
+ @Parameter(description = "Pagination: Number of items to return")  Integer limit
+);
+    @PUT
+    @Path("/org/{org}/project/{id}")
+    @Consumes({ "application/json" })
+    @Produces({ "application/json", "application/yaml" })
+    @Operation(summary = "Update Project", description = "Updates the information of the Project with the matching Project identifier.", security = {
+        @SecurityRequirement(name = "x-api-key")    }, tags={ "Project" })
+    @ApiResponses(value = { 
+        @ApiResponse(responseCode = "200", description = "Project response", content = @Content(mediaType = "application/json", schema = @Schema(implementation = ProjectResponse.class))) })
+    ProjectResponse updateProject( @PathParam("id")
+
+ @Parameter(description = "Project identifier") String id
+, @PathParam("org")
+
+ @Parameter(description = "Slug field of the organization the resource is scoped to") String org
+,@Valid UpdateProjectRequest body,  @QueryParam("account") 
+
+ @Parameter(description = "Account identifier query param")  String account
+);}
