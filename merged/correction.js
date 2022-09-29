034ad1a9443952e78a77c412b013492b4e47631a
Mankrit Singh <mankrit.singh@harness.io>
2022-09-29 10:14:28 +0530

    [PL-27473]: x-tags as a JSON

--- merged/correction.js
+++ merged/correction.js
@@ -1,33 +1,11 @@
 var fs=require('fs');
 var data=fs.readFileSync('merged/spec-first.json', 'utf8');
+var data2 = fs.readFileSync('merged/x-tags.json', 'utf8');
 var words=JSON.parse(data);
+var xTags=JSON.parse(data2);
 func()
 function func(){
-  var temp =[
-    {
-      "name": "Roles",
-      "tags": [
-        "Account Roles",
-        "Organization Roles",
-        "Project Roles"
-      ]
-    },
-    {
-    "name" : "Organization",
-    "tags" : [ "Organization" ]
-  }, {
-    "name" : "Project",
-    "tags" : [ "Org Project" ]
-  }, {
-    "name" : "Secret",
-    "tags" : [ "Account Secret", "Org Secret", "Project Secret" ]
-  },
-  {
-    "name" : "Resource Groups",
-    "tags" : [ "Account Resource Groups", "Organization Resource Groups", "Project Resource Groups" ]
-  }
-  ];
-  words['x-tagGroups'] = temp;
+  words['x-tagGroups'] = xTags;
   const x = JSON.stringify(words, null, 4);
 
   fs.writeFile('merged/spec-first.json', x, (err) => {
