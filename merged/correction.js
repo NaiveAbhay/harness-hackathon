f01dd3ca28087df733ccee30a3f7ac8edb9713e2
Mankrit Singh <mankrit.singh@harness.io>
2022-12-14 11:56:20 +0530

    [SPG-777]: clean up harness-openapi

--- merged/correction.js
+++ merged/correction.js
@@ -1,17 +1,16 @@
+const yaml = require('js-yaml');
 var fs=require('fs');
-var data=fs.readFileSync('merged/spec-first.json', 'utf8');
-var data2 = fs.readFileSync('merged/x-tags.json', 'utf8');
-var words=JSON.parse(data);
-var xTags=JSON.parse(data2);
+var words = yaml.safeLoad(fs.readFileSync('merged/spec-first.yaml', 'utf8'));
+var xTags = yaml.safeLoad(fs.readFileSync('merged/x-tags.json', 'utf8'));
 func()
 function func(){
   words['x-tagGroups'] = xTags;
-  const x = JSON.stringify(words, null, 4);
+  const yamlStr = yaml.safeDump(words)
 
-  fs.writeFile('merged/spec-first.json', x, (err) => {
+  fs.writeFile('merged/spec-first.yaml', yamlStr, (err) => {
     if (err) {
         throw err;
     }
-    console.log("JSON data is saved.");
+    console.log("YAML data is saved.");
   });
 }
