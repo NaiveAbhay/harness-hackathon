b45d0c288e9428d385780726a8c605d320537915
Mankrit Singh <mankrit.singh@harness.io>
2023-03-13 19:29:54 +0530

    path

--- _merged/correction.js
+++ _merged/correction.js
@@ -7,14 +7,14 @@
 
 const yaml = require('js-yaml');
 const fs = require('fs');
-var words = yaml.safeLoad(fs.readFileSync('_merged/merged.yaml', 'utf8'));
+var words = yaml.safeLoad(fs.readFileSync('merged.yaml', 'utf8'));
 var xTags = yaml.safeLoad(fs.readFileSync('x-tags.yaml', 'utf8'));
 func()
 function func() {
   words['x-tagGroups'] = xTags;
   const yamlStr = yaml.safeDump(words)
 
-  fs.writeFile('_merged/merged.yaml', yamlStr, (err) => {
+  fs.writeFile('merged.yaml', yamlStr, (err) => {
     if (err) {
       throw err;
     }
